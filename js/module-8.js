0325d5c2a28c7809d58fa093191a184bd6198c4d
Nikolai Koshenko <niko.koshenko@gmail.com>
2024-11-12 17:49:18 +0200

    added some exercises from JS module-8

---
+++ js/module-8.js
@@ -0,0 +1,52 @@
+// Check target element
+
+const colorPalette = document.querySelector(".color-palette");
+const output = document.querySelector(".output");
+
+colorPalette.addEventListener("click", selectColor);
+
+// This is where delegation «magic» happens
+function selectColor(event) {
+    if (event.target.nodeName !== "BUTTON") {
+           
+    return;
+  }
+    console.log(event.target);
+    
+  const selectedColor = event.target.dataset.color;
+  output.textContent = `Selected color: ${selectedColor}`;
+    output.style.color = selectedColor;
+
+    console.log(event.target.dataset);    
+}
+
+// Some helper functions to render palette items
+createPaletteItems();
+
+function createPaletteItems() {
+  const items = [];
+  for (let i = 0; i < 60; i++) {
+    const color = getRandomHexColor();
+    const item = document.createElement("button");
+    item.type = "button";
+    item.dataset.color = color;
+    item.style.backgroundColor = color;
+    item.classList.add("item");
+    items.push(item);
+  }
+  
+  colorPalette.append(...items);
+}
+console.log(colorPalette);
+
+function getRandomHexColor() {
+  const letters = "0123456789ABCDEF";
+  let color = "#";
+
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+
+
+  return color;
+}
